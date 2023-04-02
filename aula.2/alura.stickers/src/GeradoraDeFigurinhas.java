bdc47cc0722d84223ba52fb32f7b8f075130052c
J0aoD3v <jcmichelato8@gmail.com>
2023-04-02 15:45:51 -0300

    .

---
+++ aula.2/alura.stickers/src/GeradoraDeFigurinhas.java
@@ -0,0 +1,23 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class GeradoraDeFigurinhas {
+    
+
+    public void cria() {
+
+        BufferedImage imagemOrigiral = ImageIO.read(new File("entrada/Filme.jpg");
+
+        // Leitura da imagem
+
+        // Cria nova imagem em memória com transparência e com tamanho novo
+
+        // Copiar a imagem original para nova iamgem (em memória)
+
+        // Escrever uma frasse na nova imagem
+
+        // Escrever uma imagem nova em um arquivo
+    }
+}
